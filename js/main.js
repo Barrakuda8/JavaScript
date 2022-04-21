0ccb93b82af2f7c99f62a65da5d708ccd1947439
Barrakuda8 <anime.vegan@gmail.com>
2022-04-21 13:03:50 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,29 @@
+const products = [
+    {id: 1, title: 'Notebook', price: 1000},
+    {id: 2, title: 'Mouse', price: 100},
+    {id: 3, title: 'Keyboard', price: 250},
+    {id: 4, title: 'Gamepad', price: 150},
+];
+
+const getProductHTMLString = (title='product', price=0) => {
+    return `<div class="product-item">
+                <h3>${title}</h3>
+                <p>${price}</p>
+                <button class="by-btn">Добавить</button>
+              </div>`;
+};
+
+const renderProducts = (productList=[]) => {
+  /* const list = productList.map((good) => getProductHTMLString(good.title, good.price));
+
+  for (i in list) {
+      document.querySelector('.products').insertAdjacentHTML('beforeend', list[i]);
+  } */
+
+  for (i in productList) {
+      document.querySelector('.products').insertAdjacentHTML('beforeend', getProductHTMLString(productList[i].title, productList[i].price));
+  }
+  
+}
+
+renderProducts(products);
