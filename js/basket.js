95c9067b9bc88627ce59132420063892e6b6ac36
Barrakuda8 <anime.vegan@gmail.com>
2022-04-29 16:32:32 +0300

    Lesson 2 commit

---
+++ js/basket.js
@@ -0,0 +1,21 @@
+class Basket {
+    constructor() {
+    }
+
+    addGood() {
+    }
+
+    deleteGood() {
+    }
+
+    getTotalCost() {
+    }
+
+    getTotalQuantity() {
+    }
+}
+
+class Good {
+    constructor() {
+    }
+}
