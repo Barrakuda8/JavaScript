ba737fed16bcede81392bfd4658d09031ab17e08
Barrakuda8 <anime.vegan@gmail.com>
2022-05-09 12:36:13 +0300

    Lesson 3 commit

--- js/basket.js
+++ js/basket.js
@@ -1,21 +1,47 @@
+const API = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
+
 class Basket {
     constructor() {
+        this.goods = [];
+
+        this.getBasket();
     }
 
-    addGood() {
+    getBasket() {
+        return fetch(`${API}/getBasket.json`)
+        .then((data) => {for (good of data.contents) {this.addGood(good)}})
+        .catch(err => console.log(err));
     }
 
-    deleteGood() {
+    /* не понял, как конкретно нужно было использовать информацию с api для добавления и удаления, поэтому сделал так */
+
+    addGood(good) {
+        this.goods.push(new Good(good));
+    }
+
+    deleteGood(good) {
+        delete this.goods[good];
     }
 
     getTotalCost() {
+        let totalCost = 0;
+        for (good of this.goods) {
+            totalCost += good.price;
+        }
+        return totalCost;
     }
 
     getTotalQuantity() {
+        return this.goods.length;
     }
 }
 
 class Good {
-    constructor() {
+    constructor(good) {
+        this.id = good.id_product;
+        this.name = good.product_name;
+        this.price = good.price;
     }
 }
+
+new Basket();
--- js/main.js
+++ js/main.js
@@ -1,60 +1,74 @@
+const API = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
+// переписать на промис (!!!!!!!не fetch !!!!!!!!!!)
+// Далее НЕ ИСПОЛЬЗОВАТЬ В КОДЕ!
+let getRequest = (url) => {
+  let xhr = new XMLHttpRequest();
+  xhr.open('GET', url, true);
+  xhr.onreadystatechange = (a) => {
+    return new Promise((resolve, reject) => {
+        if (xhr.readyState === 4) {
+            if(a === 200) {
+                resolve(xhr.responseText);
+            } else {
+                reject('Error');
+            }
+        }
+    });
+  };
+  xhr.send();
+};
+// ---------------------------------
+
 class ProductList {
-    constructor(container = '.products') {
-        this.container = document.querySelector(container);
-        this.goods = [];
-        this.productObjects = [];
+  constructor(container = '.products') {
+    this.container = document.querySelector(container);
+    this.goods = [];
+    this.productObjects = [];
 
-        this.fetchGoods();
-        this.render();
+    this.getProducts()
+        .then((data) => {
+          this.goods = data;
+          this.render();
+        });
+  }
+
+  getProducts() {
+    return fetch(`${API}/catalogData.json`)
+        .then(response => response.json())
+        .catch(err => console.log(err));
+  }
+
+  render() {
+    for (const good of this.goods) {
+      const productObject = new ProductItem(good);
+      console.log(productObject);
+      this.productObjects.push(productObject);
+
+      this.container.insertAdjacentHTML('beforeend', productObject.getHTMLString());
     }
-
-    fetchGoods() {
-        this.goods = [
-            {id: 1, title: 'Notebook', price: 1000},
-            {id: 2, title: 'Mouse', price: 100},
-            {id: 3, title: 'Keyboard', price: 250},
-            {id: 4, title: 'Gamepad', price: 150},
-        ];
-    }
-
-    render () {
-        for(const good of this.goods) {
-            const productObject = new ProductItem(good);
-            this.productObjects.push(productObject);
-            this.container.insertAdjacentHTML('beforeend', productObject.getHTMLString());
-        }
-    }
-
-    getTotalPrice() {
-        let totalPrice = 0;
-        for (const obj of this.productObjects) {
-            totalPrice += obj.price;
-        }
-        return totalPrice;
-    };
+  }
 }
 
 class ProductItem {
-    constructor(product, img = 'https://via.placeholder.com/200x150') {
-        this.id = product.id;
-        this.title = product.title;
-        this.price = product.price;
-        this.img = img;
-    }
+  constructor(product, img='https://via.placeholder.com/200x150') {
+    this.id = product.id;
+    this.title = product.title;
+    this.price = product.price;
+    this.img = img;
+  }
 
-    getHTMLString() {
-        return `<div class='product-item' data-id='${this.id}'>
-                    <img src='${this.img}' alt='Placeholder'>
-                    <div class='desc'>
-                        <h3>${this.title}</h3>
-                        <p>${this.price}</p>
-                        <button class='by-btn'>Добавить</button>
-                    </div>
-              </div>`;
-    }
+  getHTMLString() {
+    return `<div class="product-item" data-id="${this.id}">
+                <img src="${this.img}" alt="Some img">
+                <div class="desc">
+                    <h3>${this.title}</h3>
+                    <p>${this.price} \u20bd</p>
+                    <button class="buy-btn">Купить</button>
+                </div>
+            </div>`;
+  }
 }
 
-let list = new ProductList();
-let totalPrice = list.getTotalPrice();
-console.log(totalPrice);
+new ProductList();
+
 
